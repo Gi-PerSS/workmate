a87d8a8390bd2374a3b15f22901fd26fd6e133e0
filiusro <filiusro@student.21-school.ru>
2025-07-05 14:13:23 +0300

    in progress

--- t.py
+++ t.py
@@ -1,12 +1,58 @@
 import argparse
 import sys
+import csv
 
-class MyArgumentParser(argparse.ArgumentParser):
-    """Переопределяем стандартный парсер для вывода хелпа при ошибке ввода аргументов."""
+# Парсинг CLI
+ARGUMENT_DEFINITIONS = {
+     '--file': {
+        'type': str,
+        'help': 'Путь к CSV-файлу',
+        'required': True
+    },
+    '--where': {
+        'type': str,
+        'help': 'Флаг фильтрации',
+        'required': False
+    },
+    '--aggregate': {
+        'type': str,
+        'help': 'Флаг агрегации',
+        'required': False
+    },
+    '--order-by': {
+        'type': str,
+        'help': 'Флаг порядка сортировки',
+        'required': False
+    }
+}
+
+class CLIArgumentParser(argparse.ArgumentParser):
+    """Переопределяем стандартный парсер для вывода хелпа в случае неадеквата поданных аргументов."""
     def error(self, message):
         self.print_help(sys.stderr)
         self.exit(2, f"\nОшибка: {message}\n")
 
+class CLIArgumentsDispatcher:
+    def run(self):
+        parser = CLIArgumentParser(description='Workmate. Тестовое задание')
+        for flag, params in ARGUMENT_DEFINITIONS.items():
+            parser.add_argument(flag, **params)
+        args = parser.parse_args()
+        print(f"DBG. Переданные аргументы: {vars(args)}")
+        return args
+    
+# Парсинг csv-файла
+class CSVParser:
+    """Парсер csv-файла. Считывает данные из файла и возвращает их в виде списка словарей."""
+    def __init__(self, file_path):
+        self.file_path = file_path
+
+    def parse(self):        
+        with open(self.file_path, mode='r', encoding='utf-8') as csvfile:
+            reader = csv.DictReader(csvfile)
+            return [row for row in reader]
+
+# Команды фильтрации, агрегации и прочего
 class CommandRegistry:
     """Реестр команд. Регистрируем классы-команд, обрабатывающие csv-файлы. 
     (Для удобного расширения функционала)"""
@@ -26,11 +72,6 @@ class CommandRegistry:
         return cls._registry.get(command)
 
 
-
-if __name__ == "__main":
-    parser = MyArgumentParser(description='Workmate. Тестовое задание')
-
-    parser.add_argument('--foo', type=int, help='Пример аргумента')
-
-    args = parser.parse_args()
-    print(f"DBG. All arguments: {args.foo}")
+if __name__ == "__main__":
+    t = CLIArgumentsDispatcher()
+    t.run()
